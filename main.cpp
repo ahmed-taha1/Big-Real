6eb5cd0723f79b4608eed0de1de7a228c7296412
Ahmed Taha <101575320+ahmed-taha1@users.noreply.github.com>
2022-10-30 21:25:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,21 @@
 using namespace std;
 
 int main() {
-    BigReal h("12.7600");
-    cout << h;
+    system("cls");
+    BigReal h("12.007600");
+    BigReal h2("12.07600");
+    cout << h << " " << h2 << '\n';
+    cout <<"equal : " << (h==h2) <<'\n';
+    cout <<"greater than : " << (h>h2) <<'\n';
+    cout <<"less than : " << (h<h2)  << '\n';
+    cout << '\n';
+
+    BigReal h3("12.76000000000000");
+    BigReal h4("12.7600");
+    cout << h3 << " " << h4 << '\n';
+    cout <<"equal : " << (h3==h4) <<'\n';
+    cout <<"greater than : " << (h3>h4) <<'\n';
+    cout <<"less than : " << (h3<h4)  << '\n';
+    cout << '\n';
     return 0;
 }
